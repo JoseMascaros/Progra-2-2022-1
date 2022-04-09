41042f1b69487c2cc1483d370b158b127db132f0
JossMass <jmascaro2000@gmail.com>
2022-04-09 12:48:46 -0500

    quick file fix

--- Semana 3/Motorcyclist.h
+++ Semana 3/Motorcyclist.h
@@ -4,8 +4,8 @@
 
 using namespace std;
 
-string firstNames[7] = { "Pedro" , "Juan", "Noemi", "Lucho", "Lucia", "Marko", "Vania" };
-string lastNames[6] = { "Sanchez" , "Rodriguez", "Mascaro", "Casas", "Rosas", "Rosales" };
+string firstNames[7] = { "Jorge", "Caleb", "Juan", "Alessandra", "Jean", "Jan", "Adrian" };
+string lastNames[7] = { "Rodriguez", "Coronal", "Achamizo","Ponce","Calisaya","Melgar", "Gutierrez" };
 string businessNames[2] = { "Rappi" , "Globo" };
 
 class Motorcyclist {
@@ -13,39 +13,37 @@ private:
 	string firstName;
 	string lastName;
 	int plate;
-	int kilometers;
+	int kilometer;
 	string business;
 
 public:
-	
-	//Defeault Constructor
 	Motorcyclist() {
-		firstName = firstNames[rand()% 7];
-		lastName = lastNames[rand() % 6];
+		firstName = firstNames[rand() % 7];
+		lastName = lastNames[rand() % 7];
 		plate = rand() % 20000 + 10000;
-		kilometers = rand() % 41;
+		kilometer = rand() % 41;
 		business = businessNames[rand() % 2];
 	}
 
-	//Parameter Constructor
-	Motorcyclist(string firstName, string lastName, int plate, int kilometers, string business) {
+	Motorcyclist(string firstName, string lastName, int plate, int kilometer, string business) {
 		this->firstName = firstName;
 		this->lastName = lastName;
 		this->plate = plate;
-		this->kilometers = kilometers;
+		this->kilometer = kilometer;
 		this->business = business;
 	}
 
 	~Motorcyclist(){}
 
 	double calculatePrice() {
-		return double(kilometers) * 1.5;
-	}
-
-	void getInformation() {
-		cout <<"\t " << firstName << " \t " << lastName << " \t " << plate << " \t " << kilometers << " \t " << business << " \t " << calculatePrice() << " \t " << endl;
+		return double(kilometer) * 1.5;
 	}
 
 	string getBusiness() { return business; }
 	void setBusiness(string v) { business = v; }
+
+	void getInformation() {
+		cout << "\t" << firstName << "\t" << lastName << "\t" << plate << "\t" << kilometer << "\t" << business << "\t" << calculatePrice() << "\t" << endl;
+	}
+
 };
